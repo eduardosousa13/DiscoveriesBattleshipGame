3bc70674708a35e0fe7664a8670e75e7879f32a1
Tiago <tiagopereira2005@gmail.com>
2026-02-21 17:53:18 +0000

    docs(position): add Javadocs to Position and IPosition

--- src/main/java/iscteiul/ista/battleship/IPosition.java
+++ src/main/java/iscteiul/ista/battleship/IPosition.java
@@ -1,25 +1,62 @@
-/**
- *
- */
 package iscteiul.ista.battleship;
 
 /**
- * @author fba
+ * Represents a board position identified by row and column coordinates and
+ * state flags used during gameplay.
  */
 public interface IPosition {
+    /**
+     * Gets the row index of this position.
+     *
+     * @return row index
+     */
     int getRow();
 
+    /**
+     * Gets the column index of this position.
+     *
+     * @return column index
+     */
     int getColumn();
 
+    /**
+     * Compares this position with another object.
+     *
+     * @param other object to compare with
+     * @return true when both represent the same row and column
+     */
     boolean equals(Object other);
 
+    /**
+     * Checks whether this position is adjacent to another one, including
+     * diagonal neighbors.
+     *
+     * @param other position to compare with
+     * @return true if the two positions are adjacent
+     */
     boolean isAdjacentTo(IPosition other);
 
+    /**
+     * Marks this position as occupied by part of a ship.
+     */
     void occupy();
 
+    /**
+     * Marks this position as shot.
+     */
     void shoot();
 
+    /**
+     * Indicates whether this position is occupied.
+     *
+     * @return true if occupied
+     */
     boolean isOccupied();
 
+    /**
+     * Indicates whether this position was hit by a shot.
+     *
+     * @return true if hit
+     */
     boolean isHit();
 }
